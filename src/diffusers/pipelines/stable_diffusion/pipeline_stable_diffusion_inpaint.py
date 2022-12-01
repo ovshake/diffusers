5bbecb751764248755943b57d900ae14a7f43a75
ovshake <abhishek16005@iiitd.ac.in>
2022-12-01 23:55:05 +0900

    Modification for stable fashion

--- src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_inpaint.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_inpaint.py
@@ -730,7 +730,8 @@ class StableDiffusionInpaintPipeline(DiffusionPipeline):
         image = self.decode_latents(latents)
 
         # 12. Run safety checker
-        image, has_nsfw_concept = self.run_safety_checker(image, device, text_embeddings.dtype)
+        # image, has_nsfw_concept = self.run_safety_checker(image, device, text_embeddings.dtype)
+        has_nsfw_concept = False
 
         # 13. Convert to PIL
         if output_type == "pil":
